754f550ceab386bc1c42c0ecd95f6b5390565cf0
Andre <36155981+Neaxic@users.noreply.github.com>
2021-11-15 23:24:56 +0100

    tailpik

--- SEPCSTier1/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ SEPCSTier1/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,84 +13,84 @@ namespace SEPCSTier1.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 1 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 2 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 3 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 4 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 5 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 6 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 7 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 8 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 9 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using SEPCSTier1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 10 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using SEPCSTier1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 11 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/_Imports.razor"
+#line 12 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
@@ -104,7 +104,7 @@ using Radzen.Blazor;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "/Users/lennartsargezian/RiderProjects/SEPCSTier1/SEPCSTier1/Shared/SurveyPrompt.razor"
+#line 12 "C:\Users\andre\Documents\Github\SEPCSTier1\SEPCSTier1\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
