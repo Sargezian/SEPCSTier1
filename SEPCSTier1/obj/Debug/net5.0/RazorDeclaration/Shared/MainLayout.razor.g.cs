38b0db47189f01c2edbc4bdba153646f50e1a9ce
lennartsargezian <Lennart_1997@hotmail.com>
2021-11-21 16:56:26 +0100

    Wallet og shop

--- SEPCSTier1/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ SEPCSTier1/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,91 +13,91 @@ namespace SEPCSTier1.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 1 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 2 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 3 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 4 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 5 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 6 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 7 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 8 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 9 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using SEPCSTier1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 10 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using SEPCSTier1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 11 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 12 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\Shared\MainLayout.razor"
+#line 2 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/Shared/MainLayout.razor"
 using SEPCSTier1.Pages;
 
 #line default
