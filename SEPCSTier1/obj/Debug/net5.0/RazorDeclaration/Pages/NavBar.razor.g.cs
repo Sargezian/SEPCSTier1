5c0656dc6865389095008d90fc058c490c6d5ee9
lennartsargezian <Lennart_1997@hotmail.com>
2021-11-29 20:07:33 +0100

    userid

--- SEPCSTier1/obj/Debug/net5.0/RazorDeclaration/Pages/NavBar.razor.g.cs
+++ SEPCSTier1/obj/Debug/net5.0/RazorDeclaration/Pages/NavBar.razor.g.cs
@@ -13,91 +13,91 @@ namespace SEPCSTier1.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 1 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 2 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 3 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 4 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 5 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 6 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 7 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 8 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 9 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using SEPCSTier1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 10 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using SEPCSTier1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 11 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\_Imports.razor"
+#line 12 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\Pages\NavBar.razor"
+#line 2 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/Pages/NavBar.razor"
 using SEPCSTier1.Data;
 
 #line default
@@ -112,7 +112,7 @@ using SEPCSTier1.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 44 "C:\Users\abdul\RiderProjects\SEPCSTier1\SEPCSTier1\Pages\NavBar.razor"
+#line 44 "/Users/lennartsargezian/Documents/GitHub/SEPCSTier1/SEPCSTier1/Pages/NavBar.razor"
        
 
     public async Task PerformLogout()
@@ -120,7 +120,7 @@ using SEPCSTier1.Data;
         try
         {
             ((CustomAuthenticationStateProvider) AuthenticationStateProvider).Logout();
-            NavigationManager.NavigateTo("/", true);
+            NavigationManager.NavigateTo("/shop", true);
         }
         catch (Exception e)
         {
