780f22a285d55f5f2dad50d7687e13db8e03d93e
abdullahiisse <abdullahi12351@gmail.com>
2021-12-13 18:29:24 +0100

    ...

--- SEPCSTier1/Models/Payment.cs
+++ SEPCSTier1/Models/Payment.cs
@@ -6,20 +6,16 @@ namespace SEPCSTier1.Models
     {
         public long id { get; set; }
         
-        [Required]
-        [StringLength(30, ErrorMessage = "Name too long (30 character limit).")]
+       
         public string name { get; set; }
         
-        [Required]
-        [StringLength(16, ErrorMessage = "Cardnumber too long (16 character limit).")]
+      
         public string cardnumber { get; set; }
         
-        [Required]
-        [StringLength(16, ErrorMessage = "expirationdate too long (16 character limit).")]
+       
         public string expirationdate { get; set; }
         
-        [Required]
-        [StringLength(16, ErrorMessage = "securitycode too long (3 character limit).")]
+       
         public string securitycode { get; set; }
         
         
